d597925318126d5764af709c07fe8e827b46e087
Rummantouqeer <139452817+Rummantouqeer@users.noreply.github.com>
2023-08-02 12:03:15 -0700

    Create index.js

---
+++ question7/index.js
@@ -0,0 +1,5 @@
+"use strict";
+/*Favorite Number: Store your favorite number in a variable. Then, using that variable,
+ create a message that reveals your favorite number. Print that message.*/
+var favourite_Number = 5;
+console.log("my favourite number is" + favourite_Number);
