fc0e5768eef7c476c4e31cae794dddd2d0483df6
Rummantouqeer <139452817+Rummantouqeer@users.noreply.github.com>
2023-08-02 10:21:21 -0700

    Create index.ts

---
+++ question 2/index.ts
@@ -0,0 +1,8 @@
+/*Famous Quote: Find a quote from a famous person you admire. Print the quote and the name of
+ its author. Your output should look something like the following, including the quotation marks*/
+
+ let authorName = ("Joe Jackson");
+ console.log(authorName);
+
+ let Quote = ("If you build it,they will come");
+ console.log(Quote);
