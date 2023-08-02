e207326b2de1db9c88117c5ad56528971289055f
Rummantouqeer <139452817+Rummantouqeer@users.noreply.github.com>
2023-08-02 10:42:24 -0700

    Create index.ts

---
+++ question3/index.ts
@@ -0,0 +1,7 @@
+/*Name Cases: Store a person’s name in a variable, and then print that
+ person’s name in lowercase, uppercase, and titlecase.*/
+
+var personName = ("rumman");
+console.log(personName);
+console.log(personName.toUpperCase);
+console.log(personName.toLowerCase);
