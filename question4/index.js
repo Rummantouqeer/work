86bfa3f9c6a82d4eb9aff02515e91a02813374a3
Rummantouqeer <139452817+Rummantouqeer@users.noreply.github.com>
2023-08-02 11:12:32 -0700

    Create index.js

---
+++ question4/index.js
@@ -0,0 +1,8 @@
+"use strict";
+/*Famous Quote 2: Repeat Exercise 4, but this time store the famous person’s name in a variable
+called famous_person. Then compose your message and store it in a new variable called message.
+ Print your message.*/
+var famous_person = ("Joe Jackson");
+console.log(famous_person);
+var Message = ("if you build,it will come");
+console.log(Message);
