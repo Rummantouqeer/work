0229e17e1f26da6ad83c4867b3b9ffaab34188d0
Rummantouqeer <139452817+Rummantouqeer@users.noreply.github.com>
2023-08-02 12:18:09 -0700

    Create index.js

---
+++ question 8/index.js
@@ -0,0 +1,7 @@
+"use strict";
+//Adding Comments: Choose two of the programs you’ve written, and add atleast one comment to each. If you don’t have anything specific to write because your programs are too simple at this point, just add your name and the current date at the top of each program file. Then write one sentence describing what the program does.
+var Name = ("rumman"); //here my name is stored as variable
+console.log(Name); //this will give output of my name
+const date = ("5 november"); //here const data type will store number in string
+console.log(date);
+//gives output
