586c117e47d2075818388130fa04368b19a596e7
Rummantouqeer <139452817+Rummantouqeer@users.noreply.github.com>
2023-08-02 10:22:04 -0700

    Create index.js

---
+++ question 2/index.js
@@ -0,0 +1,7 @@
+"use strict";
+/*Famous Quote: Find a quote from a famous person you admire. Print the quote and the name of
+ its author. Your output should look something like the following, including the quotation marks*/
+let authorName = ("Joe Jackson");
+console.log(authorName);
+let Quote = ("If you build it,they will come");
+console.log(Quote);
