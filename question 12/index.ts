adaaa6872da24e719126b25b216e59a0bae7f125
Rummantouqeer <139452817+Rummantouqeer@users.noreply.github.com>
2023-08-03 08:33:30 -0700

    Create index.ts

---
+++ question 12/index.ts
@@ -0,0 +1,13 @@
+/*Greetings: Start with the array you used in Exercise 11, but instead of just printing each person’s name,
+print a message to them. The text of each message should be the same, but each message should be personalized
+ with the person’s name.*/
+
+
+let FriendsArray = ["rumman","neha","fariha"];
+
+console.log(FriendsArray[0],"we are best friends")
+console.log(FriendsArray[1],"we are best friends")
+console.log(FriendsArray[2],"we are best friends")
+
+
+
