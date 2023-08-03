74fb0da10a56a33b958be4ddca89fd5f0ed2922c
Rummantouqeer <139452817+Rummantouqeer@users.noreply.github.com>
2023-08-03 08:45:31 -0700

    Create index.ts

---
+++ question 13/index.ts
@@ -0,0 +1,11 @@
+/*Your Own Array: Think of your favorite mode of transportation, such as a motorcycle or a car, and make a list that
+ stores several examples. Use your list to print a series of statements about these items, such as “I would like to 
+ own a Honda motorcycle.”*/
+
+
+
+ let myArray = ["Honda motorcycle","Toyota car","BMW motorcycle"]
+
+ console.log("i would like to own",myArray[0]);
+ console.log("i would like to own",myArray[1]);
+ console.log("i would like to own",myArray[2]);
