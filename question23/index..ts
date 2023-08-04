b7e94839f046f72e0031da1cf2e230565e5df5a7
Rummantouqeer <139452817+Rummantouqeer@users.noreply.github.com>
2023-08-04 04:27:51 -0700

    Create index..ts

---
+++ question23/index..ts
@@ -0,0 +1,23 @@
+/*Conditional Tests: Write a series of conditional tests. Print a statement describing each test and your prediction for the results of each test. 
+Your code should look something like this:*/
+
+
+//true result
+
+for (let i = 0 ; i <5 ; i++){
+    let car = 'sabaru';
+    if(car=='sabaru')
+    console.log("true")
+    else
+    console.log("false")
+ 
+}
+
+//false result
+for(let i = 0; i<5; i++){
+    let car = 'supra';
+    if(car=='sabaru')
+    console.log(true);
+    else
+    console.log(false);
+}
