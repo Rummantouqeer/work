e249e4d1c2309b4e919bfb4bc643e72ef981fd41
Rummantouqeer <139452817+Rummantouqeer@users.noreply.github.com>
2023-08-04 08:43:28 -0700

    Create index.ts

---
+++ question39/index.ts
@@ -0,0 +1,14 @@
+
+
+
+//cities:write a functioin..................
+
+
+function city_country(name:string, country:string) {
+    console.log(` ${name} , ${country} `);
+}
+
+
+city_country(`rawalpindi`, `pakistan`);
+city_country(`istanbul`, `turkey`);
+city_country(`dubai`, `saudiArabia`);
