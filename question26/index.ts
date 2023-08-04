08c0067d2c3752e074ff62bfe343e89d8bdd6657
Rummantouqeer <139452817+Rummantouqeer@users.noreply.github.com>
2023-08-04 05:08:10 -0700

    Create index.ts

---
+++ question26/index.ts
@@ -0,0 +1,7 @@
+//Alien Colors #2: Choose a color for an alien as you did in Exercise 25, and write an if-else chain.
+let alien_colors = "green,red.yellow";
+if (alien_colors=="green") {
+    console.log("player earned 5 points")
+} else {
+    console.log("player earned 10 points")
+}
