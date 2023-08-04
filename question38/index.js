89baf8360f0d624e5315552d69888d415ec60bc2
Rummantouqeer <139452817+Rummantouqeer@users.noreply.github.com>
2023-08-04 08:42:54 -0700

    Create index.js

---
+++ question38/index.js
@@ -0,0 +1,8 @@
+"use strict";
+//cities:write a functioin..................
+function describe_city(name, country) {
+    console.log(`the city ${name} lies in country ${country} `);
+}
+describe_city(`rawalpindi`, `pakistan`);
+describe_city(`istanbul`, `turkey`);
+describe_city(`dubai`, `saudiArabia`);
