e13b835cce0243490032b428fdf7d4a0d314aead
Rummantouqeer <139452817+Rummantouqeer@users.noreply.github.com>
2023-08-04 05:07:08 -0700

    Create index.js

---
+++ question25/index.js
@@ -0,0 +1,6 @@
+"use strict";
+//Alien Colors #1: Imagine an alien was just shot down in a game. Create a variable called alien_color and assign it a value of 'green', 'yellow', or 'red'.
+let alien_color = "red,green,yellow";
+if (alien_color == "green") {
+    console.log("the player got 5 points");
+}
