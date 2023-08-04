4f2fb8111f491f79c41a03fd690e584a18b4af60
Rummantouqeer <139452817+Rummantouqeer@users.noreply.github.com>
2023-08-04 08:58:31 -0700

    Create index.js

---
+++ assignment 2/index.js
@@ -0,0 +1,36 @@
+"use strict";
+//Assignment 2:
+/*Create a grading system that takes subject marks as input from the user through prompts
+ and returns the corresponding grades using functions and if-else conditions*/
+//====================================
+let subjectmarks = prompt("what are your marks");
+let totalMarks = 100;
+if (totalMarks = 100) {
+    alert("Grade A");
+}
+else if (totalMarks <= 90) {
+    alert("Grade B");
+}
+else if (totalMarks <= 80) {
+    alert("Grade C");
+}
+else {
+    alert("fail");
+}
+//=======================================================
+function subjectMarks(test1, test2) {
+    const totalMarks = test1 + test2;
+    let subjectmarks = "";
+}
+if (totalMarks <= 100) {
+    console.log("Grade A");
+}
+else if (totalMarks <= 90) {
+    console.log("Grade B");
+}
+else if (totalMarks <= 80) {
+    console.log("Grade C");
+}
+else {
+    console.error("fail");
+}
