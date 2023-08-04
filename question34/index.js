5167d2f92d256886dfaa1857964f9e43b7127e99
Rummantouqeer <139452817+Rummantouqeer@users.noreply.github.com>
2023-08-04 08:38:52 -0700

    Create index.js

---
+++ question34/index.js
@@ -0,0 +1,7 @@
+"use strict";
+//Pizzas: Think of at least three kinds of your favorite pizza. Store these pizza names in a array, and then use a for loop to print the name of each pizza.
+let pizzas = ["pepperoni pizza", "crown crust pizza", "fajita pizza"];
+for (let pizza of pizzas) {
+    console.log(`i like ${pizza} pizza`);
+}
+console.log("i really love $(pizza)pizza!");
