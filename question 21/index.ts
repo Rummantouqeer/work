b494d3197476aed4c8378093ca5c001bd7ad7dd2
Rummantouqeer <139452817+Rummantouqeer@users.noreply.github.com>
2023-08-04 04:03:17 -0700

    Create index.ts

---
+++ question 21/index.ts
@@ -0,0 +1,26 @@
+//They think of something you could store in a TypeScript Object. Write a program that creates Objects containing these items.
+
+
+
+
+
+//object of type person
+
+
+let person1name  = "john doe";
+console.log(person1name);
+const hisage = 22;
+console.log(hisage);
+var hisnationality = "british";
+console.log(hisnationality);
+
+//object of type animal
+
+let animalName = "zoro";
+console.log(animalName);
+
+var animalage = "4 years";
+console.log(animalage);
+
+var hiscolour = "brown";
+console.log(hiscolour);
