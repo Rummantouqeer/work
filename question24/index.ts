158751a1fcb2ee3290a1afb7733e70d5ead9fa2c
Rummantouqeer <139452817+Rummantouqeer@users.noreply.github.com>
2023-08-04 04:51:31 -0700

    Create index.ts

---
+++ question24/index.ts
@@ -0,0 +1,56 @@
+/*More Conditional Tests: You don’t have to limit the number of tests you create to 10. If you want to try more comparisons, write more tests. 
+Have at least one True and one False result for each of the following:*/
+
+
+// Tests for equality and inequality with strings
+
+for( let i=0;i<10;i++){
+    let string="result";
+    if(string=="result")
+    console.log(true)
+    else
+    console.log(false)
+
+}
+
+//• Tests using the lower case function
+
+for(let i =0;i<10;i++){
+    let myname = "rumman"
+    if(myname=="lowercasefunction")
+    console.log(true)
+    else
+    console.log(false)
+}
+
+//• Numerical tests involving equality and inequality, greater than and less than, greater than or equal to, and less than or equal to
+
+for(let i =0;i<10;i++){
+    let number = 30;
+    if(number>10)
+    console.log(false)
+    else
+    console.log(true)
+}
+
+//• Tests using "and" and "or" operators
+
+for(let i=0;i<10;i++){
+    let a=1
+    let b=2
+    let sum =3
+    if(a>0&&b<3)
+    console.log(true)
+    else
+    console.log(false)
+}
+
+//• Test whether an item is in a array
+
+for(let i =0;i<10;i++){
+    let pet = ["dog"]
+    console.log(pet[0])
+    if(pet[0]="array")
+    console.log(true)
+    else("false")
+}
