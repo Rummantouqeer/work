61f0c70c36f4d94b0c243bf7e0d6699a8b0e6e86
Rummantouqeer <139452817+Rummantouqeer@users.noreply.github.com>
2023-08-04 05:39:48 -0700

    Create index.ts

---
+++ question29/index.ts
@@ -0,0 +1,20 @@
+//• Make a array of your three favorite fruits and call it favorite_fruits.
+
+//• Write five if statements. Each should check whether a certain kind of fruit is in your array. If the fruit is in your array, the if block should print a statement, such as You really like bananas!
+
+let array = ["orange","kiwi","berry","grapes","banana"];
+let favfruit = "berry";
+if(favfruit=="array[0]"){
+    console.log("you really like berry")
+}
+else if(favfruit=="array[1]"){
+    console.log("you really like kiwi")
+}
+else if(favfruit=="array[2]") 
+console.log("you really like berry")
+else if (favfruit=="array[3]"){
+    console.log("you really like grapes")
+}
+else if(favfruit=="array[4]"){
+    console.log("you really like banana")
+}
