b92739e3bf81f707a315858e7ae4a044ae13defc
Rummantouqeer <139452817+Rummantouqeer@users.noreply.github.com>
2023-08-04 08:42:26 -0700

    Create index.ts

---
+++ question38/index.ts
@@ -0,0 +1,12 @@
+
+//cities:write a functioin..................
+
+
+function describe_city(name:string, country:string) {
+    console.log(`the city ${name} lies in country ${country} `);
+}
+
+
+describe_city(`rawalpindi`, `pakistan`);
+describe_city(`istanbul`, `turkey`);
+describe_city(`dubai`, `saudiArabia`);
