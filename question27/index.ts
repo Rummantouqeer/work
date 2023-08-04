29266ce62f040adbb7c291de27dcf9e7970fb3d7
Rummantouqeer <139452817+Rummantouqeer@users.noreply.github.com>
2023-08-04 05:15:04 -0700

    Create index.ts

---
+++ question27/index.ts
@@ -0,0 +1,10 @@
+//Alien Colors #3: Turn your if-else chain from Exercise 5-4 into an if-else chain.
+let AlienColour = "red";
+if(AlienColour=="green"){
+    console.log("you earned 5 points")
+}else if(AlienColour=="yellow"){
+    console.log("you earned 10 points")
+}else if(AlienColour=="red"){
+    console.log("you earned 15 points")
+}else
+console.log("you failed")
