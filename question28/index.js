c9519f071f80f3b7afb3cf49509c0f75ac45355c
Rummantouqeer <139452817+Rummantouqeer@users.noreply.github.com>
2023-08-04 05:27:33 -0700

    Create index.js

---
+++ question28/index.js
@@ -0,0 +1,17 @@
+"use strict";
+//Stages of Life: Write an if-else chain that determines a person’s stage of life. Set a value for the variable age, and then:
+var personage = "63years";
+if (personage = "2years") {
+    console.log("person is a baby");
+}
+else if (personage = "4years") {
+    console.log("person is a toddler");
+}
+else if (personage >= "13years") {
+    console.log("person is a kid");
+}
+else if (personage = "13years" || personage > "20 years") {
+    console.log("person is a teenager");
+}
+else if (personage = "30 years" || personage > "65years")
+    console.log("person is an adult");
