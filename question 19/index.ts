011cb606e4ee869cea5c5961ec8a1bf5e84eb27d
Rummantouqeer <139452817+Rummantouqeer@users.noreply.github.com>
2023-08-04 03:40:08 -0700

    Create index.ts

---
+++ question 19/index.ts
@@ -0,0 +1,11 @@
+//
+/*Dinner Guests: Working with one of the programs from Exercises 14 through 18, print a message indicating the number of people you are
+ inviting to dinner.*/
+
+
+
+
+console.log("i am inviting two guests for dinner");
+let myGuest = ["mama","papa"];
+console.log(myGuest[0] ,"and", myGuest[1], "are invited to dinner tonight!");
+console.log("number of guests are two");
