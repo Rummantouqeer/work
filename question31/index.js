742cd9c753344891797fb91deaa7f73d032c87d3
Rummantouqeer <139452817+Rummantouqeer@users.noreply.github.com>
2023-08-04 06:46:25 -0700

    Create index.js

---
+++ question31/index.js
@@ -0,0 +1,22 @@
+"use strict";
+//No Users: Add an if test to Exercise 28 to make sure the list of users is not empty.
+//• If the list is empty, print the message We need to find some users!
+var personage = "63years";
+let userlist = "empty";
+if (userlist == "empty") {
+    console.log("we need to find more users");
+    if (personage = "2years") {
+        console.log("person is a baby");
+    }
+    else if (personage = "4years") {
+        console.log("person is a toddler");
+    }
+    else if (personage >= "13years") {
+        console.log("person is a kid");
+    }
+    else if (personage = "13years" || personage > "20 years") {
+        console.log("person is a teenager");
+    }
+    else if (personage = "30 years" || personage > "65years")
+        console.log("person is an adult");
+}
