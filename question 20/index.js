4ad357118e048f76bb0eb8fdc5cbe5c36a448bd3
Rummantouqeer <139452817+Rummantouqeer@users.noreply.github.com>
2023-08-04 03:49:44 -0700

    Create index.js

---
+++ question 20/index.js
@@ -0,0 +1,10 @@
+"use strict";
+/*Think of something you could store in a array. For example, you could make a list of mountains, rivers, countries, cities, languages, or anything
+ else you’d like. Write a program that creates a list containing these items.*/
+console.log("there are 195 countries in the world");
+console.log("my favourite countries are");
+let myarray = ["turkey", "saudi arabia", "maldives", "korea"];
+console.log(myarray[0]);
+console.log(myarray[1]);
+console.log(myarray[2]);
+console.log(myarray[3]);
