ed9c775360b025366ad7d7a723fe05d9b91cf1a0
Rummantouqeer <139452817+Rummantouqeer@users.noreply.github.com>
2023-08-04 08:45:48 -0700

    Create index.js

---
+++ question41/index.js
@@ -0,0 +1,12 @@
+"use strict";
+//album...............
+function makeAlbum(artist, title, tracks) {
+    let album = { artist, title };
+    if (tracks) {
+        album.tracks = tracks;
+    }
+    return album;
+}
+console.log(makeAlbum('Pink Floyd', 'The Dark Side of the Moon'));
+console.log(makeAlbum('Led Zeppelin', 'IV'));
+console.log(makeAlbum('The Beatles', 'Abbey Road', 17));
