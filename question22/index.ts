56a983f5f7ccac19d013f6faf2c66c53062e3040
Rummantouqeer <139452817+Rummantouqeer@users.noreply.github.com>
2023-08-04 04:15:02 -0700

    Create index.ts

---
+++ question22/index.ts
@@ -0,0 +1,23 @@
+/*Intentional Error: If you haven’t received an array index error in one of your programs yet, try to make one happen
+. Change an index in one of your programs to produce an index error. Make sure you correct the error before closing the program.*/
+
+
+
+
+     //error 
+let GuestArray = ["mama","papa","saqib","mahrukh"];
+
+console.log("dear",GuestArray[0],"please come to dinner tomorrow")
+console.log("dear",GuestArray[1],"please come to dinner tomorrow")
+console.log("dear",GuestArray[2],"please come to dinner tomorrow")
+console.log("dear",GuestArray[3]"please come to dinner tomorrow)
+                                    //error
+console.log("pop method");     //error
+const removeGuest = GuestArray.po
+console.log(removeGuest);
+console.log("Array length",GuestArray.length);
+
+console.log("push method");
+const ArrayLength = GuestArray.push("grandma");
+console.log(GuestArray.push("Grandma"));
+console.log("Array length",GuestArray.length);
