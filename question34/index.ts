a5fed695fdc0bb0186233199389c1f2cd6f40a89
Rummantouqeer <139452817+Rummantouqeer@users.noreply.github.com>
2023-08-04 08:38:22 -0700

    Create index.ts

---
+++ question34/index.ts
@@ -0,0 +1,8 @@
+//Pizzas: Think of at least three kinds of your favorite pizza. Store these pizza names in a array, and then use a for loop to print the name of each pizza.
+
+
+let pizzas = ["pepperoni pizza","crown crust pizza","fajita pizza"]
+for (let pizza of pizzas){
+    console.log(`i like ${pizza} pizza`);
+}
+console.log("i really love $(pizza)pizza!");
