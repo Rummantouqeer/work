74eace5383b5489c145231d8a7da6ceaaef05ef3
Rummantouqeer <139452817+Rummantouqeer@users.noreply.github.com>
2023-08-04 08:41:54 -0700

    Create index.js

---
+++ question37/index.js
@@ -0,0 +1,8 @@
+"use strict";
+//T-Shirt: large shirts:modify......................................
+function makeshirt(size = "large", message = "i love typescript") {
+    console.log(`the thirt have size ${size} and have message ${message} print on it`);
+}
+makeshirt(`large`, `hey there`);
+makeshirt(`medium`, `cute pose`);
+makeshirt(`small`, `sweatie`);
