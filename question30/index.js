c3fff41e7f55dddfa1a09fcdb3b37316e8de49c4
Rummantouqeer <139452817+Rummantouqeer@users.noreply.github.com>
2023-08-04 06:39:09 -0700

    Create index.js

---
+++ question30/index.js
@@ -0,0 +1,12 @@
+"use strict";
+/*Hello Admin: Make a array of five or more usernames, including the name 'admin'. Imagine you are writing code that will print a greeting
+ to each user after they log in to a website. Loop through the array, and print a greeting to each user*/
+for (let i = 0; i > 5; i++) {
+    console.log("greetings");
+}
+let adminarray = ["user123", "user456", "user789", "user098", "user653"];
+console.log("hello admin!", adminarray[0]);
+console.log("hello admin!", adminarray[1]);
+console.log("hello admin!", adminarray[2]);
+console.log("hello admin!", adminarray[3]);
+console.log("hello admin!", adminarray[4]);
